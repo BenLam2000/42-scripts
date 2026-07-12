57d1f8b40ace13b77b13e679199fce101760970b
Ben Lam <benlamhsienli@gmail.com>
2026-07-12 11:46:29 +0800

    feat: check dir exists

--- discord-installer/discord_installer.sh
+++ discord-installer/discord_installer.sh
@@ -3,9 +3,15 @@
 echo -ne '\n - Downloading archive from official Discord website...\n'
 wget -O "$HOME/Downloads/discord.tar.gz" "https://discord.com/api/download?platform=linux&format=tar.gz"
 echo -ne '\n - Extracting archive to $HOME/.local/share/...\n'
+if [ ! -d $HOME/.local/share ]; then
+	mkdir -p $HOME/.local/share
+fi
 tar -xvf "$HOME/Downloads/discord.tar.gz" -C "$HOME/.local/share/"
 
-echo -ne '\n Adding script that runs discord archive script to $HOME/.local/bin/discord...'
+echo -ne '\n - Adding script that runs discord archive script to $HOME/.local/bin/discord...'
+if [ ! -d $HOME/.local/bin ]; then
+	mkdir -p $HOME/.local/bin
+fi
 cat > $HOME/.local/bin/discord << 'EOF'
 #!/usr/bin/bash
 exec nohup $HOME/.local/share/Discord/discord --no-sandbox "$@" &
